c232ce5bd77592a0dbcfb95eda395f63ada42906
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-08-20 13:27:39 +0300

    added support for raw body

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
-const {camelize, createFile, createFolder, getJson } = require('./helpers')
-const { mainDir, pageImports, urlPageContent} = require('./constants');
+const { camelize, createFile, createFolder, getJson } = require('./helpers')
+const { mainDir, pageImports, urlPageContent } = require('./constants');
 
 //import and read json file
 const item = getJson('postman.json')
@@ -10,42 +10,55 @@ const item = getJson('postman.json')
 const paramSupport = ["POST", "UPDATE"];
 const build = (dir, outerLayer) => {
     createFolder(dir);
-        let pageContent = ``;
+    let pageContent = ``;
 
-        outerLayer.item.map(innerLayer => {
-            if(typeof innerLayer.item === "undefined"){
-                const {name, request} = innerLayer;
-                const functionName = camelize(name);
-                const endpoint = request.url.path.join('/');
-                let params = [];
-                let paramString = '';
-                let shouldHaveParams = ``;
+    outerLayer.item.map(innerLayer => {
+        if (typeof innerLayer.item === "undefined") {
+            const { name, request } = innerLayer;
+            const functionName = camelize(name);
 
-                //get the parameters
-                if(typeof request.body !== undefined) {
-                    request?.body?.formdata.map(param => {
-                         params.push(param.key);
+            const endpoint = request.url?.path?.join('/');
+            let params = [];
+            let paramString = '';
+            let shouldHaveParams = ``;
+
+            //get the parameters
+            if (typeof request.body !== undefined) {
+
+                if (request?.body?.formdata) {
+                    request?.body?.formdata?.map(param => {
+                        params.push(param.key);
                     })
-                    paramString = params.join(', ');
-                    if(paramSupport.includes(request.method)) {
-                        shouldHaveParams = ` ,{ ${paramString} }`
+                } else {
+                    if (request?.body?.raw) {
+                        const raw = JSON.parse(request?.body?.raw);
+                        const rawKeys = Object.keys(raw);
+
+                        rawKeys.forEach((key) => {
+                            params.push(key)
+                        })
                     }
                 }
+                paramString = params.join(', ');
+                if (paramSupport.includes(request.method)) {
+                    shouldHaveParams = ` ,{ ${paramString} }`
+                }
+            }
 
-                const content = 
-`
+            const content =
+                `
 export const ${functionName} = (${paramString}) => {
     return axios.${request.method.toLowerCase()}(URL + '/${endpoint}'${shouldHaveParams});
 }             
 `
-                pageContent += content
-            }else {
-                build(dir + `/${innerLayer.name.toLowerCase()}`,innerLayer)
-            }
-        })
-        if(pageContent !== ``){
-            createFile(dir ,outerLayer.name, pageImports + pageContent)
+            pageContent += content
+        } else {
+            build(dir + `/${innerLayer.name.toLowerCase()}`, innerLayer)
         }
+    })
+    if (pageContent !== ``) {
+        createFile(dir, outerLayer.name, pageImports + pageContent)
+    }
 }
 //loops over the outer layer of the collectinon
 
@@ -55,14 +68,14 @@ const buildItem = (item) => {
         //check if the inner layer is a folder or if the layer
         if (typeof outerLayer.item !== 'undefined') {
             build(dir, outerLayer);
-        } 
+        }
     })
 }
 
 //create the main directory on the root level
 createFolder(mainDir);
 //create the url.js folder
-createFile(mainDir ,'url', urlPageContent)
+createFile(mainDir, 'url', urlPageContent)
 //build everything
 buildItem(item);
 
